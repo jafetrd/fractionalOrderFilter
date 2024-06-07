3c25b541da9922a1565cc28848810331ac4a24ee
jafetrd <jafetrd@gmail.com>
2024-06-07 14:07:57 -0600

    Update fractionalOrderFilter.h

--- fractionalOrderFilter.h
+++ fractionalOrderFilter.h
@@ -1,57 +1,55 @@
-#ifndef FRACTIONALORDERFILTER_H
-#define FRACTIONALORDERFILTER_H
+#ifndef FRACTIONAL_ORDER_FILTER_H
+#define FRACTIONAL_ORDER_FILTER_H
 
-#define MIN_ORDER -1.0
-#define MAX_ORDER 1.0
-#define MIN_TYPE 0
-#define MAX_TYPE 1.5
+#include <vector>
+#include <string>
+
+const double _MIN_ORDER = -1.0;
+const double _MAX_ORDER = 1.0;
+const double _MIN_TYPE = 0.0;
+const double _MAX_TYPE = 1.0;
+
+struct _GlobalConfig {
+    double _orden;
+    double _periodo;
+    double _tipo;
+};
+
+struct _Scale {
+    double _valor;
+    bool _aplicar;
+};
+
+struct _MatrixParams {
+    double* _matriz;
+    double* _h;
+    double* _g_o_h;
+    int _filas;
+    int _inicio;
+    int _fin;
+    double* _result;
+    bool _positivo;
+};
 
 class FractionalOrderFilter {
 public:
-
     FractionalOrderFilter(double orden, double periodo, double tipo);
-    void respuestaImpulso(int cantidad, double* resultado);
-    void Pade(double* num, int ordenNum, double* den, int ordenDen);
-    void Prony(double* num, int ordenNum, double* den, int ordenDen,int L);
-    void Shank(double* num, int ordenNum, double* den, int ordenDen,int L);
+    void _respuestaImpulso(int cantidad, double* respuesta);
+    void _Pade(double* num, int ordenNum, double* den, int ordenDen);
+    void _Shank(double* num, int ordenNum, double* den, int ordenDen, int cantidad);
+    void _Prony(double* num, int ordenNum, double* den, int ordenDen, int cantidad);
     
 private:
-
-    bool desdeShank;
+    _GlobalConfig _gC;
+    _Scale _Escala;
     
-    struct Config {
-        double orden;
-        double periodo;
-        double tipo;
-    };
-
-    Config gC;
-    
-    struct ganancia {
-        double valor;
-        bool aplicar;
-    };
-    
-    ganancia Escala;
-    
-    struct MatrixParams {
-        double* matriz;
-        double* h;
-        double* g_o_h;
-        int filas;
-        int inicio;
-        int fin;
-        double* result;
-        bool positivo;
-    };
-    
-    void calcularMatriz(const MatrixParams& params);
-    void gaussJordan(double* A, double* b, int N);
-    void Pade(double* h, double* num, int ordenNum, double* den, int ordenDen);
-    void numPade(double*h, double* num,double ordenNum,double* den,double ordenDen);
-    void Prony(double* h, double* num, int ordenNum, double* den, int ordenDen,int L);
-    void Shank(double* h, double* num, int ordenNum, double* den, int ordenDen,int L);
-    void ponerCeros(double* arrelgo,int size);
+    void _numPade(double* h, double* num,double ordenNum,double* den,double ordenDen);
+    void _Pade(double* h, double* num, int ordenNum, double* den, int ordenDen);
+    void _Shank(double* h, double* num, int ordenNum, double* den, int ordenDen, int cantidad);
+    void _Prony(double* h, double* num, int ordenNum, double* den, int ordenDen, int cantidad);
+    void _gaussJordan(double* A, double* b, int N);
+    void _calcularMatriz(const _MatrixParams& params);
+    void _ponerCeros(double* array, int size);
 };
 
-#endif // FRACTIONALORDERFILTER_H
+#endif
